1f834545d6d7194db0ccb898ccb85d4ba9eacc0a
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-01-15 12:28:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,19 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+
+git clone https://github.com/Kkssrr/mdisk.git /mdisk
+
+cd /mdisk
+
 pip3 install -r requirements.txt
+
 echo "Starting Bot...."
+
 python3 main.py
+
+Footer
+
+© 2023 GitHub, Inc.
+
+Footer navigation
+
+Terms
