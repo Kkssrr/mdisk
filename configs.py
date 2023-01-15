e6ed4144d8cc66d1c1f8176ec9ea24eb498cf444
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-01-15 13:25:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -29,7 +29,7 @@ Jᴜsᴛ Tʏᴘᴇ ᴀ Mᴏᴠɪᴇ Nᴀᴍᴇ 🎬**""" )
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ.
 
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅""" )
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-"1001785372970)
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001785372970")
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Real:QICMgfw8fdbfst41@cluster0.i4idyao.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001679221010"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 20))
